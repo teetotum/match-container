beb464080ca1efd480c1de9c978200221e6e2941
Martin Winkler <martinwinkler1982@googlemail.com>
2026-05-07 23:54:12 +0200

    fixed package typings w.r.t. ContainerQueryList change event

--- types.d.ts
+++ types.d.ts
@@ -6,6 +6,12 @@ declare global {
   interface ContainerQueryList extends EventTarget {
     container: string;
     matches: boolean;
+
+    addEventListener(type: 'change', callback: ((eventArgs: ContainerQueryListEvent) => unknown) | ({ handleEvent(eventArgs: ContainerQueryListEvent): unknown }) | null, options?: AddEventListenerOptions | boolean): void;
+    addEventListener(type: string, callback: EventListenerOrEventListenerObject | null, options?: AddEventListenerOptions | boolean): void;
+
+    removeEventListener(type: 'change', callback: ((eventArgs: ContainerQueryListEvent) => unknown) | ({ handleEvent(eventArgs: ContainerQueryListEvent): unknown }) | null, options?: EventListenerOptions | boolean): void;
+    removeEventListener(type: string, callback: EventListenerOrEventListenerObject | null, options?: EventListenerOptions | boolean): void;
   }
 
   interface ContainerQueryListEvent extends Event {
